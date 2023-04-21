48edd611d04b10ba337e0a24cd3f3082bd7880f4
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-04-21 23:05:04 +0200

    work with form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from 'src/app/home/home.component'
+import { LoginComponent } from 'src/app/login/login.component'
+import { TodosComponent } from 'src/app/todos/todos.component'
+import { UsersComponent } from 'src/app/users/users.component'
+import { ProfileComponent } from 'src/app/profile/profile.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'todos', component: TodosComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'profile', component: ProfileComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { TodosComponent } from './todos/todos.component'
 import { HttpClientModule } from '@angular/common/http'
+import { LoginComponent } from './login/login.component'
+import { HomeComponent } from './home/home.component'
+import { UsersComponent } from './users/users.component'
+import { ProfileComponent } from './profile/profile.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    LoginComponent,
+    HomeComponent,
+    UsersComponent,
+    ProfileComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
